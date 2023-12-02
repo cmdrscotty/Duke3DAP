813a2d330101f4db8c48c9ae485d48e0a5a230f5
randomcodegen <pathofstreaming@gmail.com>
2023-12-03 00:50:25 +0100

    Added E4L4 logic

--- levels/e4l4.py
+++ levels/e4l4.py
@@ -7,46 +7,219 @@ class E4L4(D3DLevel):
     name = "Babe Land"
     levelnum = 3
     volumenum = 3
-    keys = ["Red"]
+    keys = ["Red", "Blue"]
     location_defs = [
-        {"id": 6, "name": "6 RPG", "type": "sprite"},
-        {"id": 9, "name": "9 Chaingun", "type": "sprite"},
-        {"id": 19, "name": "19 Protective Boots", "type": "sprite"},
-        {"id": 56, "name": "56 Red Key Card", "type": "sprite"},
-        {"id": 101, "name": "101 Pipebombs", "type": "sprite"},
-        {"id": 184, "mp": True, "name": "MP 184 Protective Boots", "type": "sprite"},
-        {"id": 190, "name": "190 Pipebombs", "type": "sprite"},
-        {"id": 191, "name": "191 Jetpack", "type": "sprite"},
-        {"id": 200, "name": "200 Holo Duke", "type": "sprite"},
-        {"id": 204, "name": "204 Night Vision Goggles", "type": "sprite"},
-        {"id": 205, "name": "205 Holo Duke", "type": "sprite"},
-        {"id": 210, "name": "210 Pipebombs", "type": "sprite"},
-        {"id": 213, "mp": True, "name": "MP 213 Medkit", "type": "sprite"},
-        {"id": 214, "name": "214 Night Vision Goggles", "type": "sprite"},
-        {"id": 215, "name": "215 Armor", "type": "sprite"},
-        {"id": 218, "name": "218 Steroids", "type": "sprite"},
-        {"id": 220, "name": "220 Medkit", "type": "sprite"},
-        {"id": 229, "name": "229 Shrinker", "type": "sprite"},
-        {"id": 230, "name": "230 Freezethrower", "type": "sprite"},
-        {"id": 231, "name": "231 Atomic Health", "type": "sprite"},
-        {"id": 232, "name": "232 RPG", "type": "sprite"},
-        {"id": 233, "name": "233 Devastator", "type": "sprite"},
-        {"id": 234, "name": "234 Chaingun", "type": "sprite"},
-        {"id": 235, "name": "235 Shotgun", "type": "sprite"},
-        {"id": 236, "name": "236 Chaingun", "type": "sprite"},
-        {"id": 238, "name": "238 RPG", "type": "sprite"},
-        {"id": 245, "name": "245 Atomic Health", "type": "sprite"},
-        {"id": 246, "name": "246 Shotgun", "type": "sprite"},
-        {"id": 445, "name": "445 Blue Key Card", "type": "sprite"},
-        {"id": 634, "name": "634 Armor", "type": "sprite"},
-        {"id": 656, "name": "656 Tripbomb", "type": "sprite"},
-        {"id": 657, "name": "657 Tripbomb", "type": "sprite"},
-        {"id": 668, "name": "668 Scuba Gear", "type": "sprite"},
-        {"id": 694, "name": "694 Atomic Health", "type": "sprite"},
-        {"id": 755, "name": "755 Shrinker", "type": "sprite"},
-        {"id": 90, "name": "Secret 1", "type": "sector"},
-        {"id": 193, "name": "Secret 2", "type": "sector"},
-        {"id": 200, "name": "Secret 3", "type": "sector"},
-        {"id": 336, "name": "Secret 4", "type": "sector"},
+        {"id": 6, "name": "Blue Boat RPG", "type": "sprite"},
+        {"id": 9, "name": "Blue Boat Chaingun", "type": "sprite"},
+        {"id": 19, "name": "Blue Upper Protective Boots", "type": "sprite"},
+        {"id": 56, "name": "Red Area Hidden Red Key Card", "type": "sprite"},
+        {"id": 101, "name": "Blue Upper 2 Pipebombs", "type": "sprite"},
+        {"id": 184, "mp": True, "name": "MP Boat Top Protective Boots", "type": "sprite"},
+        {"id": 190, "name": "Castle Pipebombs", "type": "sprite"},
+        {"id": 191, "name": "Castle Jetpack", "type": "sprite"},
+        {"id": 200, "name": "Castle Holo Duke", "type": "sprite"},
+        {"id": 204, "name": "Blue Auction Night Vision Goggles", "type": "sprite"},
+        {"id": 205, "name": "Blue Upper 2 Holo Duke", "type": "sprite"},
+        {"id": 210, "name": "Upper Main Pipebombs", "type": "sprite"},
+        {"id": 213, "mp": True, "name": "MP Blue Key Medkit", "type": "sprite"},
+        {"id": 214, "name": "Red Area Hidden Night Vision Goggles", "type": "sprite"},
+        {"id": 215, "name": "Red Area Hidden Armor", "type": "sprite"},
+        {"id": 218, "name": "Pool Steroids", "type": "sprite"},
+        {"id": 220, "name": "Pool Medkit", "type": "sprite"},
+        {"id": 229, "name": "Inside Boat Shrinker", "type": "sprite"},
+        {"id": 230, "name": "Boat Top Freezethrower", "type": "sprite"},
+        {"id": 231, "name": "Castle Atomic Health", "type": "sprite"},
+        {"id": 232, "name": "Castle RPG", "type": "sprite"},
+        {"id": 233, "name": "Castle Devastator", "type": "sprite"},
+        {"id": 234, "name": "Castle Chaingun", "type": "sprite"},
+        {"id": 235, "name": "Castle Shotgun", "type": "sprite"},
+        {"id": 236, "name": "Upper Pool Chaingun", "type": "sprite"},
+        {"id": 238, "name": "Red Area Back RPG", "type": "sprite"},
+        {"id": 245, "name": "Upper Main Atomic Health", "type": "sprite"},
+        {"id": 246, "name": "Ticket Booth Shotgun", "type": "sprite"},
+        {"id": 445, "name": "Pool Price Blue Key Card", "type": "sprite"},
+        {"id": 634, "name": "Blue Upper Armor", "type": "sprite"},
+        {"id": 656, "name": "Castle Dive Tripbomb 1", "type": "sprite"},
+        {"id": 657, "name": "Castle Dive Tripbomb 2", "type": "sprite"},
+        {"id": 668, "name": "Castle Dive Scuba Gear", "type": "sprite"},
+        {"id": 694, "name": "Sky Secret Atomic Health", "type": "sprite"},
+        {"id": 755, "name": "Blue Boat Shrinker", "type": "sprite"},
+        {"id": 90, "name": "Red Area Hidden Room Secret", "type": "sector"},
+        {"id": 193, "name": "Red Explosion Secret", "type": "sector"},
+        {"id": 200, "name": "Sky Secret", "type": "sector"},
+        {"id": 336, "name": "Prison Secret", "type": "sector"},
         {"id": 0, "name": "Exit", "type": "exit"},
     ]
+    def main_region(self) -> Region:
+        r = self.rules
+        ret = self.region(
+            self.name,
+            [
+            ],
+        )
+
+        ticket_booth = self.region(
+            "Ticket Booth",
+            [
+                "Ticket Booth Shotgun",
+                "Pool Steroids",
+            ],
+        )
+        # Can use the rotating door to clip over into the ticket booth
+        self.connect(ret, ticket_booth, r.difficulty("hard") | r.jump)
+
+        main_upper = self.region(
+            "Upper Main Area",
+            [
+                "Upper Main Pipebombs",
+                "Upper Main Atomic Health",
+            ],
+        )
+        self.connect(ret, main_upper, r.jump)
+
+        pool = self.region(
+            "Pool",
+            [
+                "Pool Medkit",
+            ],
+        )
+        self.connect(ret, pool, r.can_dive)
+
+        upper_pool = self.region(
+            "Upper Pool",
+            [
+                "Upper Pool Chaingun",
+            ],
+        )
+        self.connect(ret, upper_pool, r.jump)
+
+        pool_price = self.region(
+            "Pool Price Area",
+            [
+                "Pool Price Blue Key Card",
+            ],
+        )
+        # Can sr50 from the rotating boats onto the price pillar
+        self.connect(ret, pool_price, r.difficulty("hard") | r.jump)
+
+        red_area = self.region(
+            "Red Key Area",
+            [
+            ],
+        )
+        self.connect(ret, red_area, self.red_key)
+
+        red_secret = self.region(
+            "Red Secret",
+            [
+                "Red Explosion Secret",
+            ],
+        )
+        self.connect(red_area, red_secret, r.explosives)
+
+        red_area_back = self.region(
+            "Red Area Behind Targets",
+            [
+                "Red Area Back RPG",
+            ],
+        )
+        self.connect(red_area, red_area_back, r.jump)
+        
+
+        red_area_hidden = self.region(
+            "Red Area Hidden Room",
+            [
+                "Red Area Hidden Room Secret",
+                "Red Area Hidden Night Vision Goggles",
+                "Red Area Hidden Armor",
+                "Red Area Hidden Red Key Card",
+            ],
+        )
+        self.connect(red_area, red_area_hidden, r.jump & r.can_crouch)
+
+        blue_area = self.region(
+            "Blue Key Area",
+            [
+                "MP Blue Key Medkit",
+                "Blue Boat RPG",
+                "Blue Boat Chaingun",
+                "Blue Boat Shrinker",
+                "Blue Auction Night Vision Goggles",
+            ],
+        )
+        self.connect(main_upper, blue_area, self.blue_key)
+
+        blue_upper_start = self.region(
+            "Blue Upper Area Start",
+            [
+                "Blue Upper Armor",
+                "Blue Upper Protective Boots",
+            ],
+        )
+        # Can sr50 jump up using the sign
+        self.connect(blue_area, blue_upper_start, r.jump &
+                     (r.difficulty("hard") | r.can_sprint))
+        
+        blue_upper = self.region(
+            "Blue Upper Area",
+            [
+                "Blue Upper 2 Pipebombs",
+                "Blue Upper 2 Holo Duke",
+            ],
+        )
+        self.connect(blue_area, blue_upper, r.jump)
+
+        prison_secret = self.region(
+            "Prison Secret Area",
+            [
+                "Prison Secret",
+            ],
+        )
+        self.connect(blue_area, prison_secret, r.explosives & r.jump)
+
+        castle_dive = self.region(
+            "Castle Dive Area",
+            [
+                "Castle Dive Tripbomb 1",
+                "Castle Dive Tripbomb 2",
+                "Castle Dive Scuba Gear",
+                "Inside Boat Shrinker",
+            ],
+        )
+        self.connect(blue_area, castle_dive, r.can_dive)
+
+        boat_top = self.region(
+            "Boat Top",
+            [
+                "MP Boat Top Protective Boots",
+                "Boat Top Freezethrower",
+            ],
+        )
+        self.connect(blue_area, boat_top, r.jump)
+
+        castle = self.region(
+            "Castle",
+            [
+                "Castle Pipebombs",
+                "Castle Jetpack",
+                "Castle Holo Duke",
+                "Castle Atomic Health",
+                "Castle RPG",
+                "Castle Devastator",
+                "Castle Chaingun",
+                "Castle Shotgun",
+                "Exit",
+            ],
+        )
+        self.connect(blue_area, castle, r.jump)
+
+        sky_secret = self.region(
+            "Sky Secret Area",
+            [
+                "Sky Secret",
+                "Sky Secret Atomic Health",
+            ],
+        )
+
+        # Only requires around 30 jetpack i believe
+        self.connect(blue_area, sky_secret, r.jetpack(50))
