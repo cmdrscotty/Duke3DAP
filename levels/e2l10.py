b2205bfbeea9a0921daf3496d5f69479092ce3f3
LLCoolDave <llcooldave@gmail.com>
2023-12-04 00:20:16 +0100

    E2L6, E2L10 and E2L11 logic

--- levels/e2l10.py
+++ levels/e2l10.py
@@ -7,33 +7,41 @@ class E2L10(D3DLevel):
     name = "Spin Cycle"
     levelnum = 9
     volumenum = 1
-    keys = ["Red"]
+    keys = []
     location_defs = [
-        {"id": 36, "name": "36 Shrinker", "type": "sprite"},
-        {"id": 37, "name": "37 Freezethrower", "type": "sprite"},
-        {"id": 38, "name": "38 RPG", "type": "sprite"},
-        {"id": 39, "name": "39 Chaingun", "type": "sprite"},
-        {"id": 61, "name": "61 Medkit", "type": "sprite"},
-        {"id": 141, "mp": True, "name": "MP 141 Holo Duke", "type": "sprite"},
-        {"id": 142, "mp": True, "name": "MP 142 Jetpack", "type": "sprite"},
+        {"id": 36, "name": "Beta Shrinker", "type": "sprite"},
+        {"id": 37, "name": "Gamma Freezethrower", "type": "sprite"},
+        {"id": 38, "name": "Delta RPG", "type": "sprite"},
+        {"id": 39, "name": "Alpha Chaingun", "type": "sprite"},
+        {"id": 61, "name": "Gamma Medkit", "type": "sprite"},
+        {"id": 141, "mp": True, "name": "MP Alpha Holo Duke", "type": "sprite"},
+        {"id": 142, "mp": True, "name": "MP Beta Jetpack", "type": "sprite"},
         {
             "id": 143,
             "mp": True,
-            "name": "MP 143 Night Vision Goggles",
+            "name": "MP Gamma Night Vision Goggles",
             "type": "sprite",
         },
-        {"id": 144, "mp": True, "name": "MP 144 Jetpack", "type": "sprite"},
-        {"id": 162, "name": "162 Atomic Health", "type": "sprite"},
-        {"id": 179, "name": "179 Medkit", "type": "sprite"},
-        {"id": 199, "name": "199 Atomic Health", "type": "sprite"},
-        {"id": 206, "name": "206 Shotgun", "type": "sprite"},
-        {"id": 207, "name": "207 Pipebombs", "type": "sprite"},
-        {"id": 210, "name": "210 Shotgun", "type": "sprite"},
-        {"id": 211, "name": "211 Shrinker", "type": "sprite"},
-        {"id": 224, "name": "224 Armor", "type": "sprite"},
-        {"id": 225, "name": "225 Night Vision Goggles", "type": "sprite"},
-        {"id": 226, "name": "226 Medkit", "type": "sprite"},
-        {"id": 257, "name": "257 Holo Duke", "type": "sprite"},
-        {"id": 258, "name": "258 Devastator", "type": "sprite"},
+        {"id": 144, "mp": True, "name": "MP Delta Jetpack", "type": "sprite"},
+        {"id": 162, "name": "Beta Atomic Health", "type": "sprite"},
+        {"id": 179, "name": "Alpha Medkit", "type": "sprite"},
+        {"id": 199, "name": "Delta Atomic Health", "type": "sprite"},
+        {"id": 206, "name": "Alpha Shotgun", "type": "sprite"},
+        {"id": 207, "name": "Alpha Pipebombs", "type": "sprite"},
+        {"id": 210, "name": "Delta Shotgun", "type": "sprite"},
+        {"id": 211, "name": "Delta Shrinker", "type": "sprite"},
+        {"id": 224, "name": "Alpha Armor", "type": "sprite"},
+        {"id": 225, "name": "Gamma Night Vision Goggles", "type": "sprite"},
+        {"id": 226, "name": "Beta Medkit", "type": "sprite"},
+        {"id": 257, "name": "Center Holo Duke", "type": "sprite"},
+        {"id": 258, "name": "Center Devastator", "type": "sprite"},
         {"id": 0, "name": "Exit", "type": "exit"},
     ]
+
+    def main_region(self) -> Region:
+        r = self.rules
+        # Nothing to see here
+        ret = self.region(self.name, [loc["name"] for loc in self.location_defs])
+        # Just step onto one of the enemies to cross the gap
+        self.restrict("Exit", r.jump | r.difficulty("medium"))
+        return ret
--- levels/e2l11.py
+++ levels/e2l11.py
@@ -7,27 +7,75 @@ class E2L11(D3DLevel):
     name = "Lunatic Fringe"
     levelnum = 10
     volumenum = 1
-    keys = ["Red"]
+    keys = []
     location_defs = [
-        {"id": 28, "name": "28 Shrinker", "type": "sprite"},
-        {"id": 58, "name": "58 Devastator", "type": "sprite"},
-        {"id": 63, "mp": True, "name": "MP 63 Night Vision Goggles", "type": "sprite"},
-        {"id": 92, "name": "92 Holo Duke", "type": "sprite"},
-        {"id": 93, "name": "93 RPG", "type": "sprite"},
-        {"id": 96, "name": "96 Armor", "type": "sprite"},
-        {"id": 99, "mp": True, "name": "MP 99 Jetpack", "type": "sprite"},
-        {"id": 102, "name": "102 Atomic Health", "type": "sprite"},
-        {"id": 104, "name": "104 Pipebombs", "type": "sprite"},
-        {"id": 109, "name": "109 Steroids", "type": "sprite"},
-        {"id": 111, "name": "111 Tripmine", "type": "sprite"},
-        {"id": 112, "name": "112 Medkit", "type": "sprite"},
-        {"id": 113, "mp": True, "name": "MP 113 Shrinker", "type": "sprite"},
-        {"id": 115, "name": "115 Chaingun", "type": "sprite"},
-        {"id": 123, "name": "123 Shotgun", "type": "sprite"},
-        {"id": 124, "name": "124 Freezethrower", "type": "sprite"},
-        {"id": 125, "name": "125 Shotgun", "type": "sprite"},
-        {"id": 127, "name": "127 Pipebombs", "type": "sprite"},
-        {"id": 180, "name": "180 Atomic Health", "type": "sprite"},
-        {"id": 181, "name": "181 Atomic Health", "type": "sprite"},
+        {"id": 28, "name": "Center Ring Shrinker", "type": "sprite"},
+        {"id": 58, "name": "Top Ledge Devastator", "type": "sprite"},
+        {
+            "id": 63,
+            "mp": True,
+            "name": "MP Center Ring Night Vision Goggles",
+            "type": "sprite",
+        },
+        {"id": 92, "name": "Bottom Ring Holo Duke", "type": "sprite"},
+        {"id": 93, "name": "Center Ring RPG", "type": "sprite"},
+        {"id": 96, "name": "Bottom Ring Armor", "type": "sprite"},
+        {"id": 99, "mp": True, "name": "MP Center Ring Jetpack", "type": "sprite"},
+        {"id": 102, "name": "Center Ring Atomic Health", "type": "sprite"},
+        {"id": 104, "name": "Top Ledge Pipebombs", "type": "sprite"},
+        {"id": 109, "name": "Center Ring Steroids", "type": "sprite"},
+        {"id": 111, "name": "Bottom Ring Tripmine", "type": "sprite"},
+        {"id": 112, "name": "Top Ledge Medkit", "type": "sprite"},
+        {"id": 113, "mp": True, "name": "MP Top Ledge Shrinker", "type": "sprite"},
+        {"id": 115, "name": "Center Ring Chaingun", "type": "sprite"},
+        {"id": 123, "name": "Bottom Ring Shotgun", "type": "sprite"},
+        {"id": 124, "name": "Bottom Ring Freezethrower", "type": "sprite"},
+        {"id": 125, "name": "Bottom Ring Shotgun", "type": "sprite"},
+        {"id": 127, "name": "Center Ring Pipebombs", "type": "sprite"},
+        {"id": 180, "name": "Center Top Atomic Health 1", "type": "sprite"},
+        {"id": 181, "name": "Center Top Atomic Health 2", "type": "sprite"},
         {"id": 0, "name": "Exit", "type": "exit"},
     ]
+
+    def main_region(self) -> Region:
+        r = self.rules
+        ret = self.region(
+            self.name,
+            [
+                "Top Ledge Devastator",
+                "Top Ledge Pipebombs",
+                "MP Top Ledge Shrinker",
+                "Top Ledge Medkit",
+                "Bottom Ring Holo Duke",
+                "Bottom Ring Armor",
+                "Bottom Ring Tripmine",
+                "Bottom Ring Shotgun",
+                "Bottom Ring Freezethrower",
+                "Bottom Ring Shotgun",
+            ],
+        )
+
+        center_ring = self.region(
+            "Center Top Ring",
+            [
+                "Center Ring RPG",
+                "MP Center Ring Night Vision Goggles",
+                "Center Ring Chaingun",
+                "Center Ring Steroids",
+                "Center Ring Pipebombs",
+                "Center Ring Atomic Health",
+                "MP Center Ring Jetpack",
+                "Center Ring Shrinker",
+                "Exit",
+            ],
+        )
+        # can walk across a lizard trooper from the start
+        self.connect(ret, center_ring, r.jump | r.difficulty("medium"))
+        self.restrict("Exit", r.can_shrink | r.crouch_jump)
+
+        center_top = self.region(
+            "Center Top Platform",
+            ["Center Top Atomic Health 1", "Center Top Atomic Health 2"],
+        )
+        self.connect(center_ring, center_top, r.jump)
+        return ret
