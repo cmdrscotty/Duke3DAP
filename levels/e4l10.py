31a2c51b71da379d4d0bafdd16dbb7ae579233c9
randomcodegen <pathofstreaming@gmail.com>
2023-12-03 23:37:04 +0100

    Finished up E4 logic

--- levels/e4l10.py
+++ levels/e4l10.py
@@ -7,72 +7,219 @@ class E4L10(D3DLevel):
     name = "The Queen"
     levelnum = 9
     volumenum = 3
-    keys = ["Red"]
+    keys = ["Red", "Blue", "Yellow"]
     location_defs = [
-        {"id": 29, "name": "29 Jetpack", "type": "sprite"},
-        {"id": 150, "name": "150 Protective Boots", "type": "sprite"},
-        {"id": 151, "name": "151 Protective Boots", "type": "sprite"},
-        {"id": 260, "name": "260 Atomic Health", "type": "sprite"},
-        {"id": 297, "mp": True, "name": "MP 297 Shotgun", "type": "sprite"},
-        {"id": 298, "mp": True, "name": "MP 298 Shotgun", "type": "sprite"},
-        {"id": 299, "mp": True, "name": "MP 299 Shotgun", "type": "sprite"},
-        {"id": 300, "mp": True, "name": "MP 300 Shotgun", "type": "sprite"},
-        {"id": 312, "name": "312 Pipebombs", "type": "sprite"},
-        {"id": 315, "name": "315 Pipebombs", "type": "sprite"},
-        {"id": 316, "name": "316 Pipebombs", "type": "sprite"},
-        {"id": 331, "name": "331 Medkit", "type": "sprite"},
-        {"id": 332, "name": "332 Atomic Health", "type": "sprite"},
-        {"id": 333, "name": "333 Armor", "type": "sprite"},
-        {"id": 337, "name": "337 Scuba Gear", "type": "sprite"},
-        {"id": 338, "name": "338 Protective Boots", "type": "sprite"},
-        {"id": 374, "name": "374 Armor", "type": "sprite"},
-        {"id": 375, "name": "375 Atomic Health", "type": "sprite"},
-        {"id": 388, "mp": True, "name": "MP 388 Devastator", "type": "sprite"},
-        {"id": 424, "name": "424 Medkit", "type": "sprite"},
-        {"id": 453, "name": "453 RPG", "type": "sprite"},
+        {"id": 29, "name": "R-Side Dive Jetpack", "type": "sprite"},
+        {"id": 150, "name": "R-Side Protective Boots", "type": "sprite"},
+        {"id": 151, "name": "L-Side Protective Boots", "type": "sprite"},
+        {"id": 260, "name": "Yellow Dive Atomic Health 1", "type": "sprite"},
+        {"id": 297, "mp": True, "name": "MP Start Shotgun 1", "type": "sprite"},
+        {"id": 298, "mp": True, "name": "MP Start Shotgun 2", "type": "sprite"},
+        {"id": 299, "mp": True, "name": "MP Start Shotgun 3", "type": "sprite"},
+        {"id": 300, "mp": True, "name": "MP Start Shotgun 4", "type": "sprite"},
+        {"id": 312, "name": "L-Side Vent Pipebombs", "type": "sprite"},
+        {"id": 315, "name": "Center Pipebombs 1", "type": "sprite"},
+        {"id": 316, "name": "Center Pipebombs 2", "type": "sprite"},
+        {"id": 331, "name": "Queen Medkit", "type": "sprite"},
+        {"id": 332, "name": "Queen Atomic Health", "type": "sprite"},
+        {"id": 333, "name": "Queen Armor", "type": "sprite"},
+        {"id": 337, "name": "L-Side Scuba Gear", "type": "sprite"},
+        {"id": 338, "name": "R-Side Protective Boots", "type": "sprite"},
+        {"id": 374, "name": "L-Side Dive Armor", "type": "sprite"},
+        {"id": 375, "name": "L-Side Dive Atomic Health", "type": "sprite"},
+        {"id": 388, "mp": True, "name": "MP Center Devastator 1", "type": "sprite"},
+        {"id": 424, "name": "Red Medkit", "type": "sprite"},
+        {"id": 453, "name": "R-Side RPG", "type": "sprite"},
         {"id": 561, "name": "561 Jetpack", "type": "sprite"},
-        {"id": 579, "name": "579 RPG", "type": "sprite"},
-        {"id": 613, "mp": True, "name": "MP 613 Shotgun", "type": "sprite"},
-        {"id": 614, "mp": True, "name": "MP 614 Shotgun", "type": "sprite"},
-        {"id": 615, "name": "615 Shotgun", "type": "sprite"},
-        {"id": 616, "mp": True, "name": "MP 616 Shotgun", "type": "sprite"},
-        {"id": 624, "name": "624 Jetpack", "type": "sprite"},
-        {"id": 669, "mp": True, "name": "MP 669 Devastator", "type": "sprite"},
-        {"id": 686, "name": "686 Pipebombs", "type": "sprite"},
-        {"id": 701, "mp": True, "name": "MP 701 Shotgun", "type": "sprite"},
-        {"id": 702, "mp": True, "name": "MP 702 Shotgun", "type": "sprite"},
-        {"id": 776, "name": "776 Medkit", "type": "sprite"},
-        {"id": 789, "name": "789 Blue Key Card", "type": "sprite"},
-        {"id": 823, "name": "823 Red Key Card", "type": "sprite"},
-        {"id": 824, "name": "824 Atomic Health", "type": "sprite"},
-        {"id": 825, "name": "825 Armor", "type": "sprite"},
-        {"id": 840, "name": "840 Armor", "type": "sprite"},
-        {"id": 841, "name": "841 Armor", "type": "sprite"},
-        {"id": 842, "name": "842 Yellow Key Card", "type": "sprite"},
-        {"id": 844, "name": "844 Steroids", "type": "sprite"},
-        {"id": 852, "name": "852 Atomic Health", "type": "sprite"},
-        {"id": 859, "mp": True, "name": "MP 859 Chaingun", "type": "sprite"},
-        {"id": 860, "mp": True, "name": "MP 860 Chaingun", "type": "sprite"},
-        {"id": 861, "mp": True, "name": "MP 861 Chaingun", "type": "sprite"},
-        {"id": 862, "mp": True, "name": "MP 862 Chaingun", "type": "sprite"},
-        {"id": 891, "name": "891 Pipebombs", "type": "sprite"},
-        {"id": 892, "name": "892 Pipebombs", "type": "sprite"},
-        {"id": 893, "name": "893 Pipebombs", "type": "sprite"},
-        {"id": 894, "mp": True, "name": "MP 894 Chaingun", "type": "sprite"},
-        {"id": 895, "mp": True, "name": "MP 895 Chaingun", "type": "sprite"},
-        {"id": 896, "name": "896 Chaingun", "type": "sprite"},
-        {"id": 897, "name": "897 Chaingun", "type": "sprite"},
-        {"id": 903, "name": "903 Devastator", "type": "sprite"},
-        {"id": 904, "name": "904 Devastator", "type": "sprite"},
-        {"id": 933, "name": "933 RPG", "type": "sprite"},
-        {"id": 934, "name": "934 RPG", "type": "sprite"},
-        {"id": 964, "name": "964 Pipebombs", "type": "sprite"},
-        {"id": 65, "name": "Secret 1", "type": "sector"},
-        {"id": 67, "name": "Secret 2", "type": "sector"},
-        {"id": 70, "name": "Secret 3", "type": "sector"},
-        {"id": 72, "name": "Secret 4", "type": "sector"},
-        {"id": 197, "name": "Secret 5", "type": "sector"},
-        {"id": 357, "name": "Secret 6", "type": "sector"},
-        {"id": 630, "name": "Secret 7", "type": "sector"},
+        {"id": 579, "name": "L-Side RPG", "type": "sprite"},
+        {"id": 613, "mp": True, "name": "MP Lobby Shotgun 4", "type": "sprite"},
+        {"id": 614, "mp": True, "name": "MP Lobby Shotgun 1", "type": "sprite"},
+        {"id": 615, "name": "Lobby Shotgun Shotgun 1", "type": "sprite"},
+        {"id": 616, "mp": True, "name": "MP Lobby Shotgun 3", "type": "sprite"},
+        {"id": 624, "name": "L-Side Jetpack", "type": "sprite"},
+        {"id": 669, "mp": True, "name": "MP Center Devastator 2", "type": "sprite"},
+        {"id": 686, "name": "Lobby Pipebombs 1", "type": "sprite"},
+        {"id": 701, "mp": True, "name": "MP Lobby Shotgun 5", "type": "sprite"},
+        {"id": 702, "mp": True, "name": "MP Lobby Shotgun 2", "type": "sprite"},
+        {"id": 776, "name": "Blue Medkit", "type": "sprite"},
+        {"id": 789, "name": "L-Side Dive Blue Key Card", "type": "sprite"},
+        {"id": 823, "name": "R-Side Dive Red Key Card", "type": "sprite"},
+        {"id": 824, "name": "R-Side Dive Atomic Health", "type": "sprite"},
+        {"id": 825, "name": "R-Side Dive Armor", "type": "sprite"},
+        {"id": 840, "name": "L-Side Armor", "type": "sprite"},
+        {"id": 841, "name": "R-Side Armor", "type": "sprite"},
+        {"id": 842, "name": "Red Room Yellow Key Card", "type": "sprite"},
+        {"id": 844, "name": "Yellow Steroids", "type": "sprite"},
+        {"id": 852, "name": "Yellow Dive Atomic Health 2", "type": "sprite"},
+        {"id": 859, "mp": True, "name": "MP Pillar 1 Chaingun", "type": "sprite"},
+        {"id": 860, "mp": True, "name": "MP Pillar 3 Chaingun", "type": "sprite"},
+        {"id": 861, "mp": True, "name": "MP Pillar 4 Chaingun", "type": "sprite"},
+        {"id": 862, "mp": True, "name": "MP Pillar 2 Chaingun", "type": "sprite"},
+        {"id": 891, "name": "Lobby Pipebombs 4", "type": "sprite"},
+        {"id": 892, "name": "Lobby Pipebombs 2", "type": "sprite"},
+        {"id": 893, "name": "Lobby Pipebombs 3", "type": "sprite"},
+        {"id": 894, "mp": True, "name": "MP Lobby Chaingun 2", "type": "sprite"},
+        {"id": 895, "mp": True, "name": "MP Lobby Chaingun 1", "type": "sprite"},
+        {"id": 896, "name": "Lobby Chaingun 1", "type": "sprite"},
+        {"id": 897, "name": "Lobby Chaingun 2", "type": "sprite"},
+        {"id": 903, "name": "L-Side Devastator", "type": "sprite"},
+        {"id": 904, "name": "R-Side Devastator", "type": "sprite"},
+        {"id": 933, "name": "DukeTag RPG 1", "type": "sprite"},
+        {"id": 934, "name": "DukeTag RPG 2", "type": "sprite"},
+        {"id": 964, "name": "R-Side Vent Pipebombs", "type": "sprite"},
+        {"id": 65, "name": "Pillar 1 Secret", "type": "sector"},
+        {"id": 67, "name": "Pillar 2 Secret", "type": "sector"},
+        {"id": 70, "name": "Pillar 3 Secret", "type": "sector"},
+        {"id": 72, "name": "Pillar 4 Secret", "type": "sector"},
+        {"id": 197, "name": "L-Side Vent Secret", "type": "sector"},
+        {"id": 357, "name": "R-Side Vent Secret", "type": "sector"},
+        {"id": 630, "name": "Queen Secret", "type": "sector"},
         {"id": 0, "name": "Exit", "type": "exit"},
     ]
+    def main_region(self) -> Region:
+        r = self.rules
+        ret = self.region(
+            self.name,
+            [
+                "MP Start Shotgun 1",
+                "MP Start Shotgun 2",
+                "MP Start Shotgun 3",
+                "MP Start Shotgun 4",
+                "MP Lobby Shotgun 1",
+                "MP Lobby Shotgun 2",
+                "MP Lobby Shotgun 3",
+                "MP Lobby Shotgun 4",
+                "MP Lobby Shotgun 5",
+                "Lobby Pipebombs 1",
+                "Lobby Pipebombs 2",
+                "Lobby Pipebombs 3",
+                "Lobby Pipebombs 4",
+                "Lobby Chaingun 1",
+                "Lobby Chaingun 2",
+                "MP Lobby Chaingun 1",
+                "MP Lobby Chaingun 2",
+                "Lobby Shotgun Shotgun 1",
+                "Center Pipebombs 1",
+                "Center Pipebombs 2",
+                "MP Center Devastator 1",
+                "MP Center Devastator 2",
+                "L-Side Scuba Gear",
+                "L-Side RPG",
+                "L-Side Devastator",
+            ],
+        )
+
+        lside_upper = self.region(
+            "L-Side Upper",
+            [
+                "L-Side Vent Pipebombs",
+                "L-Side Vent Secret",
+                "L-Side Armor",
+            ],
+        )
+        self.connect(ret, lside_upper, r.jump)
+
+        lside_dive = self.region(
+            "L-Side Dive",
+            [
+                "L-Side Dive Armor",
+                "L-Side Dive Atomic Health",
+                "L-Side Dive Blue Key Card",
+            ],
+        )
+        self.connect(ret, lside_dive, r.dive(1000))
+
+        blue_key_room = self.region(
+            "Blue Key Room",
+            [
+                "Blue Medkit",
+            ],
+        )
+        self.connect(ret, blue_key_room, r.jump & self.blue_key)
+        
+        rside = self.region(
+            "Right Side",
+            [
+                "R-Side Protective Boots",
+                "R-Side RPG",
+                "R-Side Devastator",
+                "R-Side Vent Secret",
+                "R-Side Vent Pipebombs",
+            ],
+        )
+        self.connect(blue_key_room, rside)
+
+        rside_upper = self.region(
+            "Right Side Upper",
+            [
+                "R-Side Armor",
+            ],
+        )
+        self.connect(rside, rside_upper, r.jump)
+
+        pillar_secrets = self.region(
+            "Pillar Secrets",
+            [
+                "Pillar 1 Secret",
+                "MP Pillar 1 Chaingun",
+                "Pillar 2 Secret",
+                "MP Pillar 2 Chaingun",
+                "Pillar 3 Secret",
+                "MP Pillar 3 Chaingun",
+                "Pillar 4 Secret",
+                "MP Pillar 4 Chaingun",
+            ],
+        )
+        # 50 might also be enough but very tight
+        self.connect(ret, pillar_secrets, r.jetpack(75))
+        
+        red_key_room = self.region(
+            "Red Key Room",
+            [
+                "Red Medkit",
+                "Red Room Yellow Key Card",
+            ],
+        )
+        self.connect(rside, red_key_room, r.jetpack & self.red_key)
+
+        rside_dive = self.region(
+            "Red Dive",
+            [
+                "R-Side Dive Jetpack", 
+                "R-Side Dive Red Key Card", 
+                "R-Side Dive Atomic Health",
+                "R-Side Dive Armor",
+            ],
+        )
+        self.connect(rside, rside_dive, r.dive(1000))
+        
+        yellow_key_room = self.region(
+            "Yellow Key Room",
+            [
+                "Yellow Steroids",
+            ],
+        )
+        self.connect(red_key_room, yellow_key_room, self.yellow_key & r.jetpack(150) |
+                     (r.crouch_jump & r.steroids))
+
+        yellow_dive = self.region(
+            "Yellow Dive",
+            [
+                "Yellow Dive Atomic Health 1",
+                "Yellow Dive Atomic Health 2",
+                "Queen Secret",
+                "Queen Medkit",
+                "Queen Atomic Health",
+                "Queen Armor",
+                "Exit",
+            ],
+        )
+        # 30 RPG Ammo required, 1600 Scuba is cutting it close
+        self.connect(yellow_key_room, yellow_dive, r.dive(1800) & r.has_group("RPG"))
+        # Unreachable DukeTag MP Locations at the Start:
+        # {"id": 933, "name": "DukeTag RPG 1", "type": "sprite"},
+        # {"id": 934, "name": "DukeTag RPG 2", "type": "sprite"},
+        # R-Side MP Only {"id": 150, "name": "R-Side Protective Boots", "type": "sprite"},
+        # R-Side MP Only 
+        # L-Side MP Only {"id": 151, "name": "L-Side Protective Boots", "type": "sprite"},
+        # L-Side MP Only {"id": 624, "name": "L-Side Jetpack", "type": "sprite"},
--- levels/e4l11.py
+++ levels/e4l11.py
@@ -7,49 +7,246 @@ class E4L11(D3DLevel):
     name = "Area 51"
     levelnum = 10
     volumenum = 3
-    keys = ["Red"]
+    keys = ["Red", "Blue", "Yellow"]
     location_defs = [
-        {"id": 50, "name": "50 Atomic Health", "type": "sprite"},
-        {"id": 90, "name": "90 Pipebombs", "type": "sprite"},
-        {"id": 94, "mp": True, "name": "MP 94 Pipebombs", "type": "sprite"},
-        {"id": 109, "mp": True, "name": "MP 109 Chaingun", "type": "sprite"},
-        {"id": 112, "mp": True, "name": "MP 112 Steroids", "type": "sprite"},
-        {"id": 198, "name": "198 Devastator", "type": "sprite"},
-        {"id": 234, "name": "234 Night Vision Goggles", "type": "sprite"},
-        {"id": 252, "name": "252 Night Vision Goggles", "type": "sprite"},
-        {"id": 254, "name": "254 Pipebombs", "type": "sprite"},
-        {"id": 258, "name": "258 Holo Duke", "type": "sprite"},
-        {"id": 275, "name": "275 Pipebombs", "type": "sprite"},
-        {"id": 294, "name": "294 Tripbomb", "type": "sprite"},
-        {"id": 295, "name": "295 Tripbomb", "type": "sprite"},
-        {"id": 297, "name": "297 Holo Duke", "type": "sprite"},
-        {"id": 298, "name": "298 Atomic Health", "type": "sprite"},
-        {"id": 299, "name": "299 Steroids", "type": "sprite"},
-        {"id": 300, "name": "300 Tripbomb", "type": "sprite"},
-        {"id": 301, "name": "301 Medkit", "type": "sprite"},
-        {"id": 302, "mp": True, "name": "MP 302 Atomic Health", "type": "sprite"},
-        {"id": 304, "name": "304 Armor", "type": "sprite"},
-        {"id": 306, "name": "306 Shotgun", "type": "sprite"},
-        {"id": 308, "name": "308 Atomic Health", "type": "sprite"},
-        {"id": 311, "name": "311 Blue Key Card", "type": "sprite"},
-        {"id": 335, "name": "335 Chaingun", "type": "sprite"},
-        {"id": 469, "name": "469 Red Key Card", "type": "sprite"},
-        {"id": 513, "name": "513 Armor", "type": "sprite"},
-        {"id": 558, "name": "558 Jetpack", "type": "sprite"},
-        {"id": 560, "name": "560 Shrinker", "type": "sprite"},
-        {"id": 561, "mp": True, "name": "MP 561 Jetpack", "type": "sprite"},
-        {"id": 568, "name": "568 RPG", "type": "sprite"},
-        {"id": 584, "name": "584 Shotgun", "type": "sprite"},
-        {"id": 610, "name": "610 Medkit", "type": "sprite"},
-        {"id": 637, "name": "637 Scuba Gear", "type": "sprite"},
-        {"id": 656, "name": "656 Freezethrower", "type": "sprite"},
-        {"id": 690, "name": "690 Yellow Key Card", "type": "sprite"},
-        {"id": 50, "name": "Secret 1", "type": "sector"},
-        {"id": 54, "name": "Secret 2", "type": "sector"},
-        {"id": 82, "name": "Secret 3", "type": "sector"},
-        {"id": 293, "name": "Secret 4", "type": "sector"},
-        {"id": 337, "name": "Secret 5", "type": "sector"},
-        {"id": 440, "name": "Secret 6", "type": "sector"},
-        {"id": 574, "name": "Secret 7", "type": "sector"},
+        {"id": 50, "name": "UFO Atomic Health", "type": "sprite"},
+        {"id": 90, "name": "Vent Entrance Pipebombs", "type": "sprite"},
+        {"id": 94, "mp": True, "name": "MP Outside Pipebombs", "type": "sprite"},
+        {"id": 109, "mp": True, "name": "MP Blue Chaingun", "type": "sprite"},
+        {"id": 112, "mp": True, "name": "MP Red Steroids", "type": "sprite"},
+        {"id": 198, "name": "Elevator Drop Devastator", "type": "sprite"},
+        {"id": 234, "name": "Cave Night Vision Goggles", "type": "sprite"},
+        {"id": 252, "name": "Blue Night Vision Goggles", "type": "sprite"},
+        {"id": 254, "name": "Outside Pipebombs", "type": "sprite"},
+        {"id": 258, "name": "Crate Holo Duke", "type": "sprite"},
+        {"id": 275, "name": "Lake Pipebombs", "type": "sprite"},
+        {"id": 294, "name": "Blue Tripbomb 1", "type": "sprite"},
+        {"id": 295, "name": "Blue Tripbomb 2", "type": "sprite"},
+        {"id": 297, "name": "Blue Holo Duke", "type": "sprite"},
+        {"id": 298, "name": "Symbol Atomic Health", "type": "sprite"},
+        {"id": 299, "name": "Corner Steroids", "type": "sprite"},
+        {"id": 300, "name": "Blue Tripbomb 3", "type": "sprite"},
+        {"id": 301, "name": "Blue Medkit", "type": "sprite"},
+        {"id": 302, "mp": True, "name": "MP Freezer Atomic Health", "type": "sprite"},
+        {"id": 304, "name": "Crate Armor", "type": "sprite"},
+        {"id": 306, "name": "Tele Cave Shotgun", "type": "sprite"},
+        {"id": 308, "name": "Vent Secret Atomic Health", "type": "sprite"},
+        {"id": 311, "name": "Vent Entrance Blue Key Card", "type": "sprite"},
+        {"id": 335, "name": "Bunker Chaingun", "type": "sprite"},
+        {"id": 469, "name": "Freezer Red Key Card", "type": "sprite"},
+        {"id": 513, "name": "Vent Entrance Armor", "type": "sprite"},
+        {"id": 558, "name": "Conveyer Jetpack", "type": "sprite"},
+        {"id": 560, "name": "Conveyer Shrinker", "type": "sprite"},
+        {"id": 561, "mp": True, "name": "MP Cave Jetpack", "type": "sprite"},
+        {"id": 568, "name": "Bridge RPG", "type": "sprite"},
+        {"id": 584, "name": "Red Shotgun", "type": "sprite"},
+        {"id": 610, "name": "Elevator Drop Medkit", "type": "sprite"},
+        {"id": 637, "name": "Crate Scuba Gear", "type": "sprite"},
+        {"id": 656, "name": "Table Freezethrower", "type": "sprite"},
+        {"id": 690, "name": "Red Area Yellow Key Card", "type": "sprite"},
+        {"id": 50, "name": "Symbol Secret", "type": "sector"},
+        {"id": 54, "name": "Corner Secret", "type": "sector"},
+        {"id": 82, "name": "Crate Secret", "type": "sector"},
+        {"id": 293, "name": "Vent Secret", "type": "sector"},
+        {"id": 337, "name": "Bridge Secret", "type": "sector"},
+        {"id": 440, "name": "Lake Secret", "type": "sector"},
+        {"id": 574, "name": "UFO Secret", "type": "sector"},
         {"id": 0, "name": "Exit", "type": "exit"},
     ]
+    def main_region(self) -> Region:
+        r = self.rules
+        ret = self.region(
+            self.name,
+            [
+                "MP Outside Pipebombs",
+                "Outside Pipebombs",
+            ],
+        )
+
+        second_area = self.region(
+            "Second Area",
+            [
+                "Bunker Chaingun",
+            ],
+        )
+        self.connect(ret, second_area, r.jetpack(50) | r.explosives)
+
+        bridge_secret = self.region(
+            "Bridge Secret",
+            [
+                "Bridge Secret",
+                "Bridge RPG",
+            ],
+        )
+        # Can also get here from lake secret
+        self.connect(second_area, bridge_secret, r.jetpack(50))
+
+        cave = self.region(
+            "Cave",
+            [
+                "MP Cave Jetpack",
+                "Cave Night Vision Goggles",
+            ],
+        )
+        self.connect(second_area, cave, r.jump)
+
+        vent_entrance = self.region(
+            "Vent Entrance",
+            [
+                "Vent Entrance Pipebombs",
+                "Vent Entrance Blue Key Card",
+                "Tele Cave Shotgun",
+                "Vent Entrance Armor",
+            ],
+        )
+        self.connect(second_area, vent_entrance, r.jump)
+
+        vent_secret = self.region(
+            "Vent Secret",
+            [
+                "Vent Secret",
+                "Vent Secret Atomic Health",
+            ],
+        )
+        self.connect(vent_entrance, vent_secret, r.jetpack(50))
+
+        blue_key_area = self.region(
+            "Blue Key Area",
+            [
+                "Blue Tripbomb 1",
+                "Blue Tripbomb 2",
+                "Blue Tripbomb 3",
+                "Blue Night Vision Goggles",
+                "MP Blue Chaingun",
+            ],
+        )
+        self.connect(second_area, blue_key_area, self.blue_key)
+        
+        blue_upper = self.region(
+            "Blue Upper Area",
+            [
+                "Symbol Secret",
+                "Symbol Atomic Health",
+                "Blue Holo Duke",
+                "Corner Secret",
+                "Corner Steroids",
+            ],
+        )
+        self.connect(blue_key_area, blue_upper, r.jump)
+
+        hidden_wall = self.region(
+            "Blue Hidden Wall",
+            [
+                "Blue Medkit",
+            ],
+        )
+        # Maybe walk + roids is also possible?  
+        self.connect(blue_key_area, hidden_wall,(r.can_sprint | r.jump))
+
+        elevator_drop  = self.region(
+            "Elevator Drop",
+            [
+                "Elevator Drop Medkit",
+                "Elevator Drop Devastator",
+            ],
+        )
+        # Can fall right onto the medkit
+        # Jetpack can be avoided but leaves the player at 25 health
+        self.connect(blue_key_area, elevator_drop, r.difficulty("hard") | 
+                     r.jetpack | 
+                     r.difficulty("medium") & (r.jump | r.jetpack))
+        
+        past_elevator  = self.region(
+            "Past Elevator",
+            [
+                "Crate Secret",
+                "Crate Armor",
+                "Crate Scuba Gear",
+            ],
+        )
+        # Can fall from elevator into opening
+        self.connect(elevator_drop, past_elevator, r.jump | r.difficulty("hard"))
+
+        lake_secret  = self.region(
+            "Lake Secret Area",
+            [
+                "Lake Secret",
+                "Lake Pipebombs",
+            ],
+        )
+        self.connect(past_elevator, lake_secret, r.explosives)
+        self.connect(lake_secret, bridge_secret)
+
+        alien_table  = self.region(
+            "Alien Table",
+            [
+                "Table Freezethrower",
+            ],
+        )
+        # Can sr50 on this without jump by using the pillar
+        self.connect(past_elevator, alien_table, r.difficulty("hard") | r.jump)
+        
+
+        alien_freezer  = self.region(
+            "Alien Freezer",
+            [
+                "MP Freezer Atomic Health",
+                "Freezer Red Key Card",
+            ],
+        )
+        self.connect(past_elevator, alien_freezer, r.can_crouch)
+
+        crates_upper  = self.region(
+            "Upper Crate Area",
+            [
+                "Crate Holo Duke",
+            ],
+        )
+        self.connect(past_elevator, crates_upper, r.jump)
+
+        conveyer_upper  = self.region(
+            "Conveyer Upper Area",
+            [
+                "Conveyer Jetpack", 
+                "Conveyer Shrinker",
+            ],
+        )
+        self.connect(past_elevator, conveyer_upper, r.jump)
+
+        ufo_secret  = self.region(
+            "UFO Secret",
+            [
+                "UFO Atomic Health",
+                "UFO Secret",
+            ],
+        )
+
+        self.connect(past_elevator, ufo_secret)
+
+        red_key_area  = self.region(
+            "Red Key Area",
+            [
+                "Red Area Yellow Key Card", 
+                "Red Shotgun",
+            ],
+        )
+        self.connect(past_elevator, red_key_area, self.red_key |
+                    r.crouch_jump & r.steroids)
+        
+        red_table = self.region(
+            "Red Table",
+            [
+                "MP Red Steroids",
+            ],
+        )
+        # Can sr50 on table
+        self.connect(red_key_area, red_table, r.difficulty("hard") |
+                     r.jump)
+        
+        yellow_key_area  = self.region(
+            "Yellow Key Area",
+            [
+                "Exit",
+            ],
+        )
+        self.connect(past_elevator, yellow_key_area, self.yellow_key)
