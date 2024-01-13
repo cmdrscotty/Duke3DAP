924b4ad952f46f18b604acdf14f9897528d8c202
LLCoolDave <llcooldave@gmail.com>
2024-01-13 21:42:23 +0100

    Finish E1 rework of logic

--- levels/e1l5.py
+++ levels/e1l5.py
@@ -9,49 +9,139 @@ class E1L5(D3DLevel):
     volumenum = 0
     keys = ["Blue"]
     location_defs = [
-        {"name": "Battlelord Medkit", "id": 4, "type": "sprite"},
-        {"name": "Battlelord Atomic Health 1", "id": 11, "type": "sprite"},
-        {"name": "Battlelord Atomic Health 2", "id": 12, "type": "sprite"},
-        {"name": "Waterfall Chaingun", "id": 24, "type": "sprite"},
+        {"name": "Battlelord Medkit", "id": 4, "type": "sprite", "density": 0},
+        {
+            "name": "Battlelord Atomic Health 1",
+            "id": 11,
+            "type": "sprite",
+            "density": 4,
+        },
+        {
+            "name": "Battlelord Atomic Health 2",
+            "id": 12,
+            "type": "sprite",
+            "density": 4,
+        },
+        {"name": "Waterfall Chaingun", "id": 24, "type": "sprite", "density": 0},
         {"name": "MP Start Chaingun", "id": 78, "type": "sprite", "density": 5},
-        {"name": "Battlelord Jetpack", "id": 79, "type": "sprite"},
-        {"name": "Battlelord Armor", "id": 80, "type": "sprite"},
-        {"name": "Cracked Wall End Atomic Health", "id": 112, "type": "sprite"},
-        {"name": "Fire Pit Pipe Bombs", "id": 131, "type": "sprite"},
-        {"name": "Top of Spaceship Atomic Health", "id": 216, "type": "sprite"},
-        {"name": "Spaceship Shaft RPG", "id": 221, "type": "sprite"},
-        {"name": "Spaceship Shaft Chaingun", "id": 222, "type": "sprite"},
-        {"name": "Fire Pit Night Vision Goggles", "id": 262, "type": "sprite"},
-        {"name": "Canyon Chaingun", "id": 291, "type": "sprite"},
+        {"name": "Battlelord Jetpack", "id": 79, "type": "sprite", "density": 3},
+        {"name": "Battlelord Armor", "id": 80, "type": "sprite", "density": 4},
+        {
+            "name": "Cracked Wall End Atomic Health",
+            "id": 112,
+            "type": "sprite",
+            "density": 4,
+        },
+        {"name": "Fire Pit Pipe Bombs", "id": 131, "type": "sprite", "density": 3},
+        {
+            "name": "Top of Spaceship Atomic Health",
+            "id": 216,
+            "type": "sprite",
+            "density": 2,
+        },
+        {"name": "Spaceship Shaft RPG", "id": 221, "type": "sprite", "density": 1},
+        {"name": "Spaceship Shaft Chaingun", "id": 222, "type": "sprite", "density": 3},
+        {
+            "name": "Fire Pit Night Vision Goggles",
+            "id": 262,
+            "type": "sprite",
+            "density": 1,
+        },
+        {"name": "Canyon Chaingun", "id": 291, "type": "sprite", "density": 3},
         {"name": "MP Secret Fire Pit Armor", "id": 294, "type": "sprite", "density": 5},
-        {"name": "Pipebombs near Blue Gate", "id": 311, "type": "sprite"},
-        {"name": "Start Shotgun", "id": 316, "type": "sprite"},
-        {"name": "Earthquake Atomic Health 1", "id": 373, "type": "sprite"},
-        {"name": "Earthquake Atomic Health 2", "id": 374, "type": "sprite"},
-        {"name": "Blue Key Card", "id": 375, "type": "sprite"},
-        {"name": "Cracked Wall Middle Atomic Health", "id": 410, "type": "sprite"},
-        {"name": "Fire Pit RPG", "id": 439, "type": "sprite"},
-        {"name": "Dancing Shaman Atomic Health 1", "id": 466, "type": "sprite"},
-        {"name": "Dancing Shaman Atomic Health 2", "id": 472, "type": "sprite"},
-        {"name": "Dancing Shaman Atomic Health 3", "id": 473, "type": "sprite"},
-        {"name": "Red Waterfall Pipe Bombs", "id": 524, "type": "sprite"},
-        {"name": "Start Protective Boots", "id": 604, "type": "sprite"},
-        {"name": "Bottom of Ruins Protective Boots", "id": 682, "type": "sprite"},
-        {"name": "Fire Place Medkit", "id": 689, "type": "sprite"},
-        {"name": "Toxic River Protective Boots", "id": 698, "type": "sprite"},
-        {"name": "Start RPG", "id": 718, "type": "sprite"},
-        {"name": "Secret Fire Pit Night Vision Goggles", "id": 726, "type": "sprite"},
-        {"name": "Secret Fire Pit Medkit", "id": 727, "type": "sprite"},
-        {"name": "Toxic River Holo Duke", "id": 733, "type": "sprite"},
-        {"name": "Toxic River Medkit", "id": 734, "type": "sprite"},
-        {"name": "Ruins Staircase Shotgun", "id": 737, "type": "sprite"},
-        {"name": "Fire Pit Steroids", "id": 738, "type": "sprite"},
-        {"name": "Cracked Wall Bottom Atomic Health", "id": 740, "type": "sprite"},
-        {"name": "Cracked Wall Protective Boots", "id": 741, "type": "sprite"},
-        {"name": "Cracked Wall Medkit", "id": 742, "type": "sprite"},
-        {"name": "Spaceship Entrance Armor", "id": 744, "type": "sprite"},
-        {"name": "Earthquake Night Vision Goggles", "id": 817, "type": "sprite"},
-        {"name": "Red Waterfall Cave Protective Boots", "id": 868, "type": "sprite"},
+        {"name": "Pipebombs near Blue Gate", "id": 311, "type": "sprite", "density": 1},
+        {"name": "Start Shotgun", "id": 316, "type": "sprite", "density": 1},
+        {
+            "name": "Earthquake Atomic Health 1",
+            "id": 373,
+            "type": "sprite",
+            "density": 1,
+        },
+        {
+            "name": "Earthquake Atomic Health 2",
+            "id": 374,
+            "type": "sprite",
+            "density": 3,
+        },
+        {"name": "Blue Key Card", "id": 375, "type": "sprite", "density": 0},
+        {
+            "name": "Cracked Wall Middle Atomic Health",
+            "id": 410,
+            "type": "sprite",
+            "density": 0,
+        },
+        {"name": "Fire Pit RPG", "id": 439, "type": "sprite", "density": 0},
+        {
+            "name": "Dancing Shaman Atomic Health 1",
+            "id": 466,
+            "type": "sprite",
+            "density": 2,
+        },
+        {
+            "name": "Dancing Shaman Atomic Health 2",
+            "id": 472,
+            "type": "sprite",
+            "density": 4,
+        },
+        {
+            "name": "Dancing Shaman Atomic Health 3",
+            "id": 473,
+            "type": "sprite",
+            "density": 4,
+        },
+        {"name": "Red Waterfall Pipe Bombs", "id": 524, "type": "sprite", "density": 0},
+        {"name": "Start Protective Boots", "id": 604, "type": "sprite", "density": 0},
+        {
+            "name": "Bottom of Ruins Protective Boots",
+            "id": 682,
+            "type": "sprite",
+            "density": 1,
+        },
+        {"name": "Fire Place Medkit", "id": 689, "type": "sprite", "density": 0},
+        {
+            "name": "Toxic River Protective Boots",
+            "id": 698,
+            "type": "sprite",
+            "density": 4,
+        },
+        {"name": "Start RPG", "id": 718, "type": "sprite", "density": 3},
+        {
+            "name": "Secret Fire Pit Night Vision Goggles",
+            "id": 726,
+            "type": "sprite",
+            "density": 3,
+        },
+        {"name": "Secret Fire Pit Medkit", "id": 727, "type": "sprite", "density": 0},
+        {"name": "Toxic River Holo Duke", "id": 733, "type": "sprite", "density": 4},
+        {"name": "Toxic River Medkit", "id": 734, "type": "sprite", "density": 1},
+        {"name": "Ruins Staircase Shotgun", "id": 737, "type": "sprite", "density": 1},
+        {"name": "Fire Pit Steroids", "id": 738, "type": "sprite", "density": 2},
+        {
+            "name": "Cracked Wall Bottom Atomic Health",
+            "id": 740,
+            "type": "sprite",
+            "density": 3,
+        },
+        {
+            "name": "Cracked Wall Protective Boots",
+            "id": 741,
+            "type": "sprite",
+            "density": 3,
+        },
+        {"name": "Cracked Wall Medkit", "id": 742, "type": "sprite", "density": 1},
+        {"name": "Spaceship Entrance Armor", "id": 744, "type": "sprite", "density": 3},
+        {
+            "name": "Earthquake Night Vision Goggles",
+            "id": 817,
+            "type": "sprite",
+            "density": 3,
+        },
+        {
+            "name": "Red Waterfall Cave Protective Boots",
+            "id": 868,
+            "type": "sprite",
+            "density": 3,
+        },
         {
             "name": "MP Shrinker Room Steroids",
             "id": 876,
@@ -64,7 +154,7 @@ class E1L5(D3DLevel):
             "type": "sprite",
             "density": 5,
         },
-        {"name": "Canyon Pillars RPG", "id": 884, "type": "sprite"},
+        {"name": "Canyon Pillars RPG", "id": 884, "type": "sprite", "density": 0},
         {"name": "MP Fire Pit Jetpack", "id": 1032, "type": "sprite", "density": 5},
         {"name": "MP Fire Place RPG", "id": 1039, "type": "sprite", "density": 5},
         {
@@ -85,9 +175,19 @@ class E1L5(D3DLevel):
             "type": "sprite",
             "density": 5,
         },
-        {"name": "Fire Pit Atomic Health", "id": 1046, "type": "sprite"},
-        {"name": "Secret Fire Pit Atomic Health 1", "id": 1047, "type": "sprite"},
-        {"name": "Secret Fire Pit Atomic Health 2", "id": 1048, "type": "sprite"},
+        {"name": "Fire Pit Atomic Health", "id": 1046, "type": "sprite", "density": 3},
+        {
+            "name": "Secret Fire Pit Atomic Health 1",
+            "id": 1047,
+            "type": "sprite",
+            "density": 2,
+        },
+        {
+            "name": "Secret Fire Pit Atomic Health 2",
+            "id": 1048,
+            "type": "sprite",
+            "density": 4,
+        },
         {"name": "Secret Red Waterfall Passage", "id": 7, "type": "sector"},
         {"name": "Secret Dancing Shaman", "id": 21, "type": "sector"},
         {"name": "Secret Top of Spaceship", "id": 257, "type": "sector"},
@@ -116,10 +216,14 @@ class E1L5(D3DLevel):
                 "Earthquake Night Vision Goggles",
             ],
         )
-        self.restrict("Blue Key Card", r.jump)  # It's juust out of reach
+        # Can pick it up pushing into the pillar from the back left at just the right angle
+        self.restrict("Blue Key Card", r.jump | r.difficulty("medium"))
         self.restrict("Earthquake Night Vision Goggles", r.jump)
+        self.restrict("Start Protective Boots", r.can_open)
 
         blue_gate = self.region("Beyond Blue Key Gate", ["Pipebombs near Blue Gate"])
+        # Gate opens automatically, no Open required
+        # Crouch jump has a reliable setup, very hard to time without one
         self.connect(
             ret,
             blue_gate,
@@ -145,41 +249,78 @@ class E1L5(D3DLevel):
                 "Earthquake Atomic Health 2",
                 "Ruins Staircase Shotgun",
                 "MP Fire Place RPG",
-                "Fire Place Medkit",
                 "MP Ruins Staircase Atomic Health",
                 "MP Ruins Staircase Armor",
-                "Fire Pit Steroids",
-                "Secret Fire Pit Hidden Wall",
                 "Fire Pit Pipe Bombs",
                 "Fire Pit Atomic Health",
                 "Secret Fire Pit Teleporter",
-                "MP Shrinker Room Steroids",
                 "Bottom of Ruins Protective Boots",
                 "MP Bottom of Ruins Shotgun",
+                "Fire Pit RPG",
+                "Waterfall Chaingun",
             ],
         )
         self.connect(fault_trigger, beyond_fault, self.event("Earthquake"))
+        # can clip through, skipping the hand trigger
+        self.restrict("Fire Pit RPG", (r.jump & r.can_use) | r.crouch_jump)
+        # Can't find a SR50 setup for this without sprint speed
+        self.restrict("Waterfall Chaingun", r.jump | r.can_sprint | r.steroids)
+
+        fault_doors = self.region(
+            "Doors after Earthquake",
+            ["Fire Pit Steroids", "Secret Fire Pit Hidden Wall"],
+        )
+        self.connect(beyond_fault, fault_doors, r.can_open)
+
+        fault_buttons = self.region("Buttons after Earthquake", ["Fire Place Medkit"])
+        self.connect(beyond_fault, fault_buttons, r.can_use)
+
+        secret_fire_pit_ledge = self.region(
+            "Secret Fire Pit Ledge",
+            [
+                "Secret Fire Pit Atomic Health 1",
+                "Secret Fire Pit Atomic Health 2",
+                "MP Secret Fire Pit Armor",
+            ],
+        )
+        # you can clip up here somehow, I just don't know how to reproduce it
+        self.connect(
+            beyond_fault, secret_fire_pit_ledge, r.jump | r.difficulty("extreme")
+        )
 
         small_ledges = self.region(
             "Ledges after Earthquake",
             [
                 "MP Ruins Staircase Atomic Health",
                 "MP Ruins Staircase Armor",
-                "Secret Fire Pit Atomic Health 1",
-                "Secret Fire Pit Atomic Health 2",
-                "MP Secret Fire Pit Armor",
                 "Secret Fire Pit Ledge",
-                "Fire Pit RPG",
-                "Waterfall Chaingun",
-                "Canyon Explosion",
-                "Fire Pit Night Vision Goggles",
-                "MP Fire Pit Jetpack",
             ],
         )
         # always have like 20 jetpack left from earthquake at this point, should all be fine
         self.connect(beyond_fault, small_ledges, r.jump)
-        # Would get stuck in the room afterward otherwise
-        self.restrict("Canyon Explosion", r.can_shrink | r.crouch_jump)
+
+        fire_pit_ledge = self.region(
+            "Fire Pit Ledge", ["Fire Pit Night Vision Goggles", "MP Fire Pit Jetpack"]
+        )
+        # Can SR50 over with sprint speed
+        self.connect(
+            beyond_fault,
+            fire_pit_ledge,
+            r.jump
+            | (r.can_sprint & (r.steroids | r.difficulty("hard")))
+            | (r.steroids & r.difficulty("extreme")),
+        )
+
+        shrinker_ledge = self.region(
+            "Shrinker Room Ledge", ["MP Shrinker Room Steroids", "Canyon Explosion"]
+        )
+        self.connect(
+            beyond_fault, shrinker_ledge, r.can_use | (r.crouch_jump & r.jetpack(50))
+        )
+        # Would get stuck in the room afterward otherwise if we can't leave
+        self.restrict(
+            "Canyon Explosion", (r.can_shrink & r.jump & r.can_use) | r.crouch_jump
+        )
 
         # anything beyond here on pure jetpack uses 50 fuel to get to the red canyon area and make progress
         # it might be possible in just 50 to get here, but that seems a harsh requirement
@@ -188,15 +329,27 @@ class E1L5(D3DLevel):
             [
                 "Red Waterfall Cave Protective Boots",
                 "Red Waterfall Pipe Bombs",
+                "Secret Red Waterfall Passage",
+            ],
+        )
+        # some more leniency on easy logic
+        self.connect(
+            beyond_fault,
+            red_waterfall,
+            r.can_jump | r.jetpack(150) | (r.jetpack(100) & r.difficulty("medium")),
+        )
+        self.restrict("Secret Red Waterfall Passage", r.explosives)
+
+        shaman_secret = self.region(
+            "Shaman Secret",
+            [
                 "Dancing Shaman Atomic Health 1",
                 "Dancing Shaman Atomic Health 2",
                 "Dancing Shaman Atomic Health 3",
                 "Secret Dancing Shaman",
-                "Secret Red Waterfall Passage",
             ],
         )
-        self.connect(beyond_fault, red_waterfall, r.can_jump | r.jetpack(100))
-        self.restrict("Secret Red Waterfall Passage", r.explosives)
+        self.connect(red_waterfall, shaman_secret, r.can_use)
 
         ship_entrance = self.region(
             "Ship Entrance",
@@ -209,14 +362,24 @@ class E1L5(D3DLevel):
                 "Cracked Wall End Atomic Health",
                 "Canyon Pillars RPG",
                 "Spaceship Entrance Armor",
-                "Secret Top of Spaceship",
-                "Top of Spaceship Atomic Health",
             ],
         )
+        # some leniency on easy
         self.connect(
             beyond_fault,
             ship_entrance,
-            self.event("Canyon Explosion") & (r.can_jump | r.jetpack(100)),
+            self.event("Canyon Explosion")
+            & (r.can_jump | r.jetpack(150) | (r.jetpack(100) & r.difficulty("medium"))),
+        )
+
+        top_of_spaceship = self.region(
+            "Top of Spaceship",
+            ["Secret Top of Spaceship", "Top of Spaceship Atomic Health"],
+        )
+        self.connect(
+            ship_entrance,
+            top_of_spaceship,
+            r.can_open | r.jetpack(250) | (r.jetpack(200) & r.difficulty("medium")),
         )
 
         spaceship = self.region(
@@ -233,7 +396,7 @@ class E1L5(D3DLevel):
             ],
         )
         # Big drop, can't get back
-        self.connect(ship_entrance, spaceship, r.true)
+        self.connect(ship_entrance, spaceship, r.can_open)
         # Clip to DM exit near boss drop
         self.restrict(
             "Exit",
