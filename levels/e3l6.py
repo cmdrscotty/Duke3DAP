5f24fe007b35995b2505317139d9c7214c1a4485
LLCoolDave <llcooldave@gmail.com>
2024-01-14 19:39:12 +0100

    E3L6 Update

--- levels/e3l6.py
+++ levels/e3l6.py
@@ -9,40 +9,50 @@ class E3L6(D3DLevel):
     volumenum = 2
     keys = ["Blue", "Red"]
     location_defs = [
-        {"id": 1, "name": "Track Steroids", "type": "sprite"},
-        {"id": 8, "name": "Red Key Card", "type": "sprite"},
-        {"id": 20, "name": "Blue Key Freezethrower", "type": "sprite"},
-        {"id": 21, "name": "Library Pipebombs", "type": "sprite"},
-        {"id": 53, "name": "Vending Machine Atomic Health", "type": "sprite"},
-        {"id": 91, "name": "Underwater Shrinker", "type": "sprite"},
-        {"id": 92, "name": "Bookshelf Atomic Health", "type": "sprite"},
-        {"id": 140, "name": "Track Devastator", "type": "sprite"},
-        {"id": 283, "name": "Magazine Store Night Vision Goggles", "type": "sprite"},
-        {"id": 296, "name": "Blue Key Card", "type": "sprite"},
-        {"id": 333, "name": "Entrance Shotgun", "type": "sprite"},
-        {"id": 334, "name": "Poster Chaingun", "type": "sprite"},
-        {"id": 339, "name": "Magazine Holo Duke", "type": "sprite"},
-        {"id": 342, "name": "Entrance Pipebombs", "type": "sprite"},
-        {"id": 361, "name": "Blue Key Armor", "type": "sprite"},
-        {"id": 362, "name": "Blue Key Medkit", "type": "sprite"},
-        {"id": 364, "name": "Blue Door Atomic Health", "type": "sprite"},
-        {"id": 365, "name": "Train Chaingun", "type": "sprite"},
-        {"id": 366, "name": "Train Atomic Health", "type": "sprite"},
-        {"id": 367, "name": "Train RPG", "type": "sprite"},
-        {"id": 368, "name": "Train Medkit", "type": "sprite"},
-        {"id": 385, "name": "Blue Door Tripmine 1", "type": "sprite"},
-        {"id": 386, "name": "Blue Door Tripmine 2", "type": "sprite"},
-        {"id": 387, "name": "Blue Door Tripmine 3", "type": "sprite"},
-        {"id": 388, "name": "Blue Door Tripmine 4", "type": "sprite"},
-        {"id": 389, "name": "Blue Door Tripmine 5", "type": "sprite"},
+        {"id": 1, "name": "Track Steroids", "type": "sprite", "density": 2},
+        {"id": 8, "name": "Red Key Card", "type": "sprite", "density": 0},
+        {"id": 20, "name": "Blue Key Freezethrower", "type": "sprite", "density": 3},
+        {"id": 21, "name": "Library Pipebombs", "type": "sprite", "density": 3},
+        {
+            "id": 53,
+            "name": "Vending Machine Atomic Health",
+            "type": "sprite",
+            "density": 0,
+        },
+        {"id": 91, "name": "Underwater Shrinker", "type": "sprite", "density": 0},
+        {"id": 92, "name": "Bookshelf Atomic Health", "type": "sprite", "density": 2},
+        {"id": 140, "name": "Track Devastator", "type": "sprite", "density": 1},
+        {
+            "id": 283,
+            "name": "Magazine Store Night Vision Goggles",
+            "type": "sprite",
+            "density": 1,
+        },
+        {"id": 296, "name": "Blue Key Card", "type": "sprite", "density": 0},
+        {"id": 333, "name": "Entrance Shotgun", "type": "sprite", "density": 1},
+        {"id": 334, "name": "Poster Chaingun", "type": "sprite", "density": 2},
+        {"id": 339, "name": "Magazine Holo Duke", "type": "sprite", "density": 2},
+        {"id": 342, "name": "Entrance Pipebombs", "type": "sprite", "density": 4},
+        {"id": 361, "name": "Blue Key Armor", "type": "sprite", "density": 4},
+        {"id": 362, "name": "Blue Key Medkit", "type": "sprite", "density": 4},
+        {"id": 364, "name": "Blue Door Atomic Health", "type": "sprite", "density": 3},
+        {"id": 365, "name": "Train Chaingun", "type": "sprite", "density": 0},
+        {"id": 366, "name": "Train Atomic Health", "type": "sprite", "density": 4},
+        {"id": 367, "name": "Train RPG", "type": "sprite", "density": 3},
+        {"id": 368, "name": "Train Medkit", "type": "sprite", "density": 4},
+        {"id": 385, "name": "Blue Door Tripmine 1", "type": "sprite", "density": 3},
+        {"id": 386, "name": "Blue Door Tripmine 2", "type": "sprite", "density": 4},
+        {"id": 387, "name": "Blue Door Tripmine 3", "type": "sprite", "density": 4},
+        {"id": 388, "name": "Blue Door Tripmine 4", "type": "sprite", "density": 4},
+        {"id": 389, "name": "Blue Door Tripmine 5", "type": "sprite", "density": 0},
         {
             "id": 411,
-            "density": 5,
             "name": "MP Blue Door Night Vision Goggles",
             "type": "sprite",
+            "density": 5,
         },
-        {"id": 429, "name": "Alien Base Devastator", "type": "sprite"},
-        {"id": 472, "name": "Track Atomic Health", "type": "sprite"},
+        {"id": 429, "name": "Alien Base Devastator", "type": "sprite", "density": 2},
+        {"id": 472, "name": "Track Atomic Health", "type": "sprite", "density": 4},
         {"id": 6, "name": "Secret Post Box Wall", "type": "sector"},
         {"id": 132, "name": "Secret Poster", "type": "sector"},
         {"id": 149, "name": "Secret Bookshelves", "type": "sector"},
@@ -73,6 +83,8 @@ class E3L6(D3DLevel):
                 "Blue Door Tripmine 5",
             ],
         )
+        self.restrict("Track Devastator", r.can_open)
+        self.restrict("Vending Machine Atomic Health", r.can_open)
 
         magazine_secret = self.region(
             "Behind the Magazine Machine", ["Secret Magazines", "Magazine Holo Duke"]
@@ -83,8 +95,6 @@ class E3L6(D3DLevel):
             "Subway Ledges",
             [
                 "Magazine Store Night Vision Goggles",
-                "Secret Poster",
-                "Poster Chaingun",
                 "Blue Key Card",
                 "Blue Key Freezethrower",
                 "Blue Key Medkit",
@@ -92,6 +102,16 @@ class E3L6(D3DLevel):
             ],
         )
         self.connect(ret, start_ledges, r.jump)
+        self.restrict("Magazine Store Night Vision Goggles", r.can_use)
+
+        duke_poster = self.region("Duke Poster", ["Secret Poster", "Poster Chaingun"])
+        self.connect(
+            ret,
+            duke_poster,
+            (r.jump & r.can_open)
+            | (r.difficulty("hard") & r.crouch_jump)
+            | r.fast_crouch_jump,
+        )
 
         track_wall = self.region(
             "Track Breakable Wall",
@@ -106,22 +126,33 @@ class E3L6(D3DLevel):
                 "MP Blue Door Night Vision Goggles",
                 "Blue Door Atomic Health",
                 "Secret Post Box Wall",
-                "Secret Alien Base",
-                "Alien Base Devastator",
             ],
         )
-        # Can auto kick the door open because it's a multi-part door connected as a switch
-        self.connect(ret, blue_door, self.blue_key | r.glitch_kick)
-        self.restrict("Secret Post Box Wall", r.jump)
+        self.connect(ret, blue_door, (self.blue_key & r.can_open) | r.glitch_kick)
+        self.restrict(
+            "Secret Post Box Wall",
+            r.jump & (r.can_open | (r.glitched & r.can_crouch & r.fast_sprint)),
+        )
+
+        alien_base = self.region(
+            "Alien Base", ["Secret Alien Base", "Alien Base Devastator"]
+        )
+        self.connect(blue_door, alien_base, r.can_open)
+        self.restrict("Alien Base Devastator", r.can_use)
 
         library = self.region("Library", ["Library Pipebombs"])
-        self.connect(ret, library, self.red_key)
+        self.connect(ret, library, self.red_key & r.can_open)
 
         bookshelf = self.region(
             "Bookshelves", ["Secret Bookshelves", "Bookshelf Atomic Health"]
         )
-        self.connect(library, bookshelf, r.jetpack(50) | (r.can_jump & r.explosives))
+        self.connect(
+            library,
+            bookshelf,
+            r.jetpack(50) | (r.can_jump & (r.explosives | r.difficulty("medium"))),
+        )
 
         exit_area = self.region("Exit Area", ["Exit", "Underwater Shrinker"])
         self.connect(library, exit_area, r.can_dive)
+        self.restrict("Exit", r.can_use)
         return ret
