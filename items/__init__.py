2f10a2be1c469e351f849da50f0e301ab85d2f5e
cmdrscotty <cmdr.scotty25@gmail.com>
2025-07-30 09:47:12 -0700

    Additional Fixes to inventory items
    Updated items to add Persistent tag to fix some gameplay issues.
    
    * Holo Duke
    * Night Vision Goggles
    * First Aid Kit (non progression)
    * Protective Boots
    
    These items could be found and granted through checks, now they'll stay in your inventory rather than disappearing as soon as a new level/save is loaded.

--- items/__init__.py
+++ items/__init__.py
@@ -493,6 +493,8 @@ inventory_items = {
         net_id(303),
         "inventory",
         {"invnum": 3, "capacity": 600},
+        persistent=True,
+        unique=True,
     ),
     "Jetpack": ItemDef(
         "Jetpack",
@@ -531,12 +533,16 @@ inventory_items = {
         net_id(307),
         "inventory",
         {"invnum": 7, "capacity": 400, "max_capacity": 1600},
+        persistent=True,
+        unique=True,
     ),
     "First Aid Kit": ItemDef(
         "First Aid Kit",
         net_id(309),
         "inventory",
         {"invnum": 9, "capacity": 35},
+        persistent=True,
+        unique=True,
     ),
     "First Aid Kit Capacity": ItemDef(
         "First Aid Kit Capacity",
@@ -560,6 +566,7 @@ inventory_items = {
         net_id(310),
         "inventory",
         {"invnum": 10, "capacity": 100, "max_capacity": 300},
+        persistent=True,
     ),
 }
 
